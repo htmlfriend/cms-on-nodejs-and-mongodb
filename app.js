c39ace6856895b97cf64480cbd8624d199351d67
yurii <yuriy.tim@yahoo.com>
2020-07-05 00:52:32 -0400

    added create and edit pages

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const bodyParser = require("body-parser");
 const exphbrs = require("express-handlebars");
+const Handlebars = require("handlebars");
+const {
+  allowInsecurePrototypeAccess,
+} = require("@handlebars/allow-prototype-access");
 const mongoose = require("mongoose");
+// for put requests
+const methodOverride = require("method-override");
 
 //routers
 const homeRoutes = require("./routes/home");
 const adminRoutes = require("./routes/admin");
 const postsRoutes = require("./routes/admin/posts");
 
+// mongoose.Promise = global.Promise;
+
 mongoose
   .connect("mongodb://localhost:27017/cms", {
-    useMongoClient: true,
     useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
   .then((db) => {
     console.log("Connect to mongodb");
@@ -22,17 +31,28 @@ mongoose
   });
 
 app.use(express.static(path.join(__dirname, "public")));
+// helper function
+
+const { select } = require("./helpers/handlebars-helpers");
 app.engine(
   "hbs",
   exphbrs({
     defaultLayout: "home",
     extname: "hbs",
+    helpers: { select: select },
     // helpers: require("./utils/hbs-helpers"),
-    // handlebars: allowInsecurePrototypeAccess(Handlebars),
+    handlebars: allowInsecurePrototypeAccess(Handlebars),
   })
 );
 app.set("view engine", "hbs");
 app.set("views", "views");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//method Override for edit post
+app.use(methodOverride("_method"));
+
 app.use("/", homeRoutes);
 app.use("/admin", adminRoutes);
 app.use("/admin/posts", postsRoutes);
--- routes/admin/posts.js
+++ routes/admin/posts.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const Post = require("../../models/Post");
 
 router.all("*", (req, res, next) => {
   req.app.locals.layout = "admin";
@@ -7,11 +8,62 @@ router.all("*", (req, res, next) => {
 });
 
 router.get("/", (req, res) => {
-  res.send("It work");
+  Post.find({}, function (err, posts) {
+    res.render("admin/posts", { posts: posts });
+  });
 });
 
-router.get("/dashboard", (req, res) => {
-  res.render("admin/dashboard");
+router.get("/create", (req, res) => {
+  res.render("admin/posts/create");
 });
 
+router.post("/create", (req, res) => {
+  let allowCommentsProp = true;
+  if (req.body.allowComments) {
+    allowCommentsProp = true;
+  } else {
+    allowCommentsProp = false;
+  }
+  const newPost = new Post({
+    title: req.body.title,
+    status: req.body.status,
+    allowComments: allowCommentsProp,
+    body: req.body.body,
+  });
+  newPost
+    .save()
+    .then((savedPost) => {
+      res.redirect("/admin/posts");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.get("/edit/:id", (req, res) => {
+  let postId = req.params.id;
+  Post.findOne({ _id: postId }).then((post) => {
+    res.render("admin/posts/edit", { post: post });
+  });
+
+  router.put("/edit/:id", (req, res) => {
+    let postId = req.params.id;
+    Post.findOne({ _id: postId }).then((post) => {
+      let allowCommentsProp = true;
+      if (req.body.allowComments) {
+        allowCommentsProp = true;
+      } else {
+        allowCommentsProp = false;
+      }
+      post.title = req.body.title;
+      post.allowComments = allowCommentsProp;
+      post.status = req.body.status;
+      post.body = req.body.body;
+
+      post.save().then((editPost) => {
+        res.redirect("/admin/posts");
+      });
+    });
+  });
+});
 module.exports = router;
